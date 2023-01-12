059d9157816f3d30853d7de8cd03eea46c9eae2b
Nehaa3 <90745234+Nehaa3@users.noreply.github.com>
2023-01-12 17:01:49 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -72,7 +72,7 @@ const handleSubmit = async(e)=>{
   loader(messageDiv);
 
   //fetch data from server i.e. bot's response
-  const response= await fetch('http://localhost:5100',{
+  const response= await fetch('https://neha.onrender.com',{
     method:'POST',
     headers:{
       'Content-Type': 'application/json'
